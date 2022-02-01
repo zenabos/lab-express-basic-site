caa203d59b8846b38c19ee621d01916ef6af1818
Zena Bos <zta.bos@gmail.com>
2022-02-01 16:29:15 +0100

    add basic functionalities

---
+++ app.js
@@ -0,0 +1,16 @@
+const { response } = require("express");
+const express = require("express");
+
+const app = express();
+
+app.use(express.static("public"));
+
+app.get("/", (req, res, next) => {
+  res.sendFile(__dirname + "/views/home.html");
+});
+
+app.get("/about", (req, res, next) => {
+  res.sendFile(__dirname + "/views/about.html");
+});
+
+app.listen(3000);
